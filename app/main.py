11559a2f591c63d651836fe0617669128aa25cfe
Sofa <sofo4kavakula@gmail.com>
2025-10-28 12:40:57 +0200

    Fix annotations in add, iadd and truediv

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Distance:
     def __init__(self, km: float) -> None:
         self.km = km
@@ -8,12 +11,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> "Distance":
+    def __add__(self, other: Distance | int | float) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: int | float) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
         else:
@@ -23,7 +26,7 @@ class Distance:
     def __mul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> bool:
+    def __truediv__(self, other: int | float) -> "Distance":
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float) -> bool:

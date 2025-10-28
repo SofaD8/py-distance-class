ed04a7b6af16837e71eae45e9d945113e688756a
Sofa <sofo4kavakula@gmail.com>
2025-10-28 12:17:47 +0200

    Fix truediv annotation

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ class Distance:
     def __mul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: int | float) -> "Distance":
+    def __truediv__(self, other: int | float) -> bool:
         return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: int | float) -> bool:
